2b9275b70fee23d179f7d02d1205089a24bd57a8
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-02-19 11:12:04 +0700

    Add interactions data to visits table schema
    - updated existing schema to have a compound PK index of `url` and `time` fields
    - `url` still seems to work ok as a FK to pages table
    - all these fields are optional, and created later, so added convenience method to wrap that updating up, using the compound index to perform the lookup

--- src/storage.js
+++ src/storage.js
@@ -1,6 +1,15 @@
 import Dexie from 'dexie'
 import relationships from 'dexie-relationships'
 
+/**
+ * @typedef {Object} VisitInteraction
+ * @property {number} duration Time user was active during visit (ms).
+ * @property {number} scrollPx Y-axis pixel scrolled to at point in time.
+ * @property {number} scrollPerc
+ * @property {number} scrollMaxPx Furthest y-axis pixel scrolled to during visit.
+ * @property {number} scrollMaxPerc
+ */
+
 export default class Storage extends Dexie {
     /**
      * @property {Dexie.Table} Represents page data - our main data type.
@@ -31,7 +40,8 @@ export default class Storage extends Dexie {
     _initSchema() {
         this.version(1).stores({
             pages: 'url,text,*tokens',
-            visits: '++,url -> pages.url,time',
+            visits:
+                '[url+time],url -> pages.url,duration,scrollMaxPerc,scrollMaxPx,scrollPerc,scrollPx',
         })
 
         // ... add versions/migration logic here
@@ -39,6 +49,8 @@ export default class Storage extends Dexie {
 
     /**
      * Performs async clearing of each table in succession (may just `Promise.all` this).
+     *
+     * @return {Promise<void>}
      */
     async clearData() {
         for (const table of this.tables) {
@@ -59,6 +71,21 @@ export default class Storage extends Dexie {
                 this.pages.add(page)
                 this.visits.add({ url: page.url, time })
             }
+        })
+    }
+
+    /**
+     * Updates an existing specified visit with interactions data.
+     *
+     * @param {string} url The URL of the visit to get.
+     * @param {string|number} time
+     * @param {VisitInteraction} data
+     * @return {Promise<void>}
+     */
+    updateVisitInteractionData(url, time, data) {
+        return this.transaction('rw', this.visits, () => {
+            // Should be a compound PK index on `url` and `time`
+            this.visits.where({ url, time }).modify({ ...data })
         })
     }
 
